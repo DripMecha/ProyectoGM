384a244eb1824659a1f58e8cf733cdb8d6320fd2
Gabriel Mayanga Cabrera <gabrielmayanga3@gmail.com>
2026-09-13 23:47:54 -0500

    fix: Incluir contenido completo front y back

---
+++ proyectoGM/src/app/features/inicio-publico.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-inicio-publico',
+  imports: [RouterLink],
+  templateUrl: './inicio-publico.html',
+  styleUrl: './inicio-publico.scss'
+})
+export class InicioPublicoComponent {}
