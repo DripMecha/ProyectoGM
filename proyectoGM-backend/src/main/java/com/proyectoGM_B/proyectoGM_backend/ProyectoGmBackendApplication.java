183f3da888c725b849199a504553e99ecf55eea3
Gabriel Mayanga Cabrera <gabrielmayanga3@gmail.com>
2026-09-13 23:37:32 -0500

    feat: Commit inicial para el proyecto fullstack

---
+++ proyectoGM-backend/src/main/java/com/proyectoGM_B/proyectoGM_backend/ProyectoGmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoGM_B.proyectoGM_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGmBackendApplication.class, args);
+	}
+
+}
---
+++ proyectoGM-backend/src/test/java/com/proyectoGM_B/proyectoGM_backend/ProyectoGmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoGM_B.proyectoGM_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
